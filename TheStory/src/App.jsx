77e26933ffe97d8e2d99ac3c8bcfb39ae6ab5402
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-11-23 16:41:41 +0200

    Routes

--- TheStory/src/App.jsx
+++ TheStory/src/App.jsx
@@ -1,17 +1,28 @@
-import './App.css'
-import Home from './components/home/Home.jsx'
-import Header from './components/home/header/Header.jsx'
-import Footer from './components/home/footer/Footer.jsx'
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
+import Contacts from "./components/contacts/Contacts.jsx";
+
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
-    <Header />
-      <Home />
-    <Footer />
+    <div className="App">
+      <Header />
+
+      <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/contacts" element={<Contacts />}/>
+      </Routes>
+
+      <Footer />
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
