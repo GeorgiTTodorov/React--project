8fa065987c9e3bfde9efb44f4f1ff8df1e7ef01e
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-12-02 13:56:39 +0200

    About page & styles

--- TheStory/src/components/about/About.jsx
+++ TheStory/src/components/about/About.jsx
@@ -1,9 +1,16 @@
+import styles from './About.module.css'
+
 export default function About() {
 
     return (
-        <>
-        <h2>About page</h2>
-        </>
+        <section className={styles.about}>
+            <h2>About this site</h2>
+            <p>
+                This site was built using React for a project assignment.
+                It is meant to be used for educational purposes and testing of functionalities.
+                You can view the repository on: <a href="https://github.com/GeorgiTTodorov/React--project">GitHub</a>
+            </p>
+        </section>
     );
 
 }
