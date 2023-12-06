42388a307ea8f312fa3576dd6817ca40bc4d0e32
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-12-06 18:28:53 +0200

    Added to Routes - Story details & Image Details

--- TheStory/src/App.jsx
+++ TheStory/src/App.jsx
@@ -9,11 +9,13 @@ import Login from "./components/login/LoginForm.jsx";
 import Register from "./components/register/RegisterForm.jsx";
 import LearnMore from "./components/learn-more/LearnMore.jsx";
 import Gallery from "./components/gallery/Gallery.jsx";
+import ImageDetails from "./components/image-details/ImageDetails.jsx";
 import Stories from "./components/stories/Stories.jsx";
 import HermitStories from "./components/hermit-stories/HermitStories.jsx";
 import CruelStories from "./components/cruel-stories/CruelStories.jsx";
 import BraveStories from "./components/brave-stories/BraveStories.jsx";
 import CreateStory from "./components/create-story/CreateStory.jsx";
+import StoryDetails from "./components/story-details/StoryDetails.jsx";
 
 import "./App.css";
 
@@ -31,7 +33,9 @@ function App() {
             <Route path="/register" element={<Register />}/>
             <Route path="/learn-more" element={<LearnMore />}/>
             <Route path="/gallery" element={<Gallery />} />
+            <Route path="/gallery/:objectId" element={<ImageDetails />} />
             <Route path="/stories" element={<Stories />} />
+            <Route path="/stories/:objectId" element={<StoryDetails />} />
             <Route path="/stories/hermit-stories" element={<HermitStories />} />
             <Route path="/stories/cruel-stories" element={<CruelStories />} />
             <Route path="/stories/brave-stories" element={<BraveStories />} />
