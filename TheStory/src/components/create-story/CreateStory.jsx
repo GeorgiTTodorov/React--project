a9cd9872a4e9cd25631fd6046a8c1f8e69e96917
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-12-06 10:02:54 +0200

    Added Create Story Form component

--- TheStory/src/components/create-story/CreateStory.jsx
+++ TheStory/src/components/create-story/CreateStory.jsx
@@ -4,8 +4,29 @@ import styles from './CreateStory.module.css';
 export default function CreateStory () {
 
     return (
+    <>
+        <BackButton />
         <section className={styles.createStory}>
-            <BackButton />
+            
+                <h2>Create Story</h2>
+
+                <form className='story'>
+                    <div className='inputs'>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" />
+                        <label htmlFor="title">Image:</label>
+                        <input type="text" placeholder='Place an image URL here'/>
+                        <label htmlFor="title">Genre:</label>
+                        <input type="text" />
+                    </div>
+                    <div className='textarea'>
+                        <label htmlFor="text">Text:</label>
+                        <textarea name="text" id="text" cols="40" rows="15"></textarea>
+                        <button>Create Story</button>
+                    </div>
+                </form>
+            
         </section>
+    </>
     );
 }
